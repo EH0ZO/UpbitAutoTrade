23b2169c346897c694ee0306dee0897b58bad686
HoeTaeYoung <gjxod7901@naver.com>
2021-05-10 00:51:10 +0900

    1분에 1회 삭제

--- main.py
+++ main.py
@@ -55,33 +55,31 @@ while True:
 
     # 매매 logic
         now = datetime.datetime.now()
-        if now.minute != minBackup:
-            for i in range(0, 10):
-                tkr = tkr_top10[i]
-                # 매수
-                if (get_balance(tkr_top10[i],"KRW") < 5000) and balance > 5000:
-                    current = get_current_price(tkr)
-                    ma15_old = get_ma(tkr, "minute1", 15, 2)
-                    ma15 = get_ma(tkr, "minute1", 15, 1)
-                    ma30_old = get_ma(tkr, "minute1", 30, 2)
-                    ma30 = get_ma(tkr, "minute1", 30, 1)
-                    # 기준선보다 높으면 매수
-                    if ma15 > ma15_old and ma30 > ma30_old and current > ma30 and now - datetime.timedelta(minutes=10) > last_sell_time[i]:
-                        buy(tkr, balance)
-                        num_buy += 1
-                # 매도
-                elif get_balance(tkr_top10[i],"KRW") > 5000:
-                    current = get_current_price(tkr)
-                    ma15 = get_ma(tkr, "minute1", 15, 1)
-                    ma30 = get_ma(tkr, "minute1", 30, 1)
-                    # 기준선보다 낮으면 매도
-                    if current < ma15 or current < ma30 :
-                        sell(tkr)
-                        num_sell += 1
-                        last_sell_time[i] = now
-                time.sleep(0.1)
-            time.sleep(1)      
-            minBackup = now.minute
+        for i in range(0, 10):
+            tkr = tkr_top10[i]
+            # 매수
+            if (get_balance(tkr_top10[i],"KRW") < 5000) and balance > 5000:
+                current = get_current_price(tkr)
+                ma15_old = get_ma(tkr, "minute1", 15, 2)
+                ma15 = get_ma(tkr, "minute1", 15, 1)
+                ma30_old = get_ma(tkr, "minute1", 30, 2)
+                ma30 = get_ma(tkr, "minute1", 30, 1)
+                # 기준선보다 높으면 매수
+                if ma15 > ma15_old and ma30 > ma30_old and current > ma30 and now - datetime.timedelta(minutes=10) > last_sell_time[i]:
+                    buy(tkr, balance)
+                    num_buy += 1
+            # 매도
+            elif get_balance(tkr_top10[i],"KRW") > 5000:
+                current = get_current_price(tkr)
+                ma15 = get_ma(tkr, "minute1", 15, 1)
+                ma30 = get_ma(tkr, "minute1", 30, 1)
+                # 기준선보다 낮으면 매도
+                if current < ma15 or current < ma30 :
+                    sell(tkr)
+                    num_sell += 1
+                    last_sell_time[i] = now
+            time.sleep(0.1)
+        time.sleep(1)      
         
 
     except Exception as e:
