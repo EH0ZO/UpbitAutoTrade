6f4ca1e823ef36e43cba851bb984afc45a534bf2
Ty <gjxod7901@naver.com>
2021-05-10 12:22:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vars_funcs import *
 # Main Logic
 
 # 로그인
-fStart = timeBackup = minBackup = num_buy = num_sell = 0
+fStart = timeBackup = num_buy = num_sell = 0
 # 시작 메세지 슬랙 전송
 post_message(myToken, myChannel, "==================================")
 post_message(myToken, myChannel, "autotrade start (ver."+VERSION+"))")
@@ -11,73 +11,68 @@ post_message(myToken, myChannel, "==================================")
 while True:
     try:
         now = datetime.datetime.now()
-    # 매일 09시 마다 신규 종목 선정
-        if timeBackup != now.hour or fStart == 0:
-            if now.hour == 9 or fStart == 0:
-                last_sell_time = [now - datetime.timedelta(minutes=10)]*10
-            # 신규 종목 선정 및 목표가 계산
-                post_message(myToken, myChannel, "=== 종목 선정 시작 : "+str(datetime.datetime.now()))
-                tkr_top10 = select_tkrs()
-                post_message(myToken, myChannel, "=== 종목 선정 완료 : "+str(datetime.datetime.now()))
-                post_message(myToken, myChannel, str(tkr_top10))
-            # 탈락 종목 전량 매도
-                post_message(myToken, myChannel, "=== 미포함 종목 매도 : "+str(datetime.datetime.now()))
-                balances = upbit.get_balances()
+
+    # 1시간 마다 신규 종목 선정
+        if(timeBackup != now.hour):
+        # 신규 종목 선정 및 목표가 계산
+            post_message(myToken, myChannel, "=== 종목 선정 시작 : "+str(datetime.datetime.now()))
+            tkr_top10 = select_tkrs()
+            for i in range(0,10):
+                # 목표가 : 전 시간 종가 + (전 시간 고점 - 저점) * K
+                ret = get_target_prce(tkr_top10[i])
+                buy_price[i] = ret[0]
+                sell_price[i] = ret[1]
                 time.sleep(0.1)
-                for b in balances:
-                    if b['currency'] != 'KRW':
-                        tkr = "KRW-"+b['currency']
-                        if tkr not in tkr_top10:
-                            if get_balance(tkr,"KRW") > 5000:
-                                sell(tkr)
-                                num_sell += 1
-                            time.sleep(0.1)
-            # 잔고 Update
-                startBalance = get_totalKRW()
-                hourlyBalance = startBalance
-
-        # 1시간 마다 매매 결과 송신
-            curBalance = get_totalKRW()
-            balChange_hr = curBalance - hourlyBalance
-            balChngPercent_hr = balChange_hr / hourlyBalance * 100
-            balChange_d = curBalance - startBalance
-            balChngPercent_d = balChange_d / startBalance * 100
-            post_message(myToken, myChannel, "=== Hourly Report ===")
+            post_message(myToken, myChannel, "=== 종목 선정 완료 : "+str(datetime.datetime.now()))
+            post_message(myToken, myChannel, str(tkr_top10))
+        # 탈락 종목 전량 매도
+            post_message(myToken, myChannel, "=== 탈락 종목 매도 : "+str(datetime.datetime.now()))
+            balances = upbit.get_balances()
+            time.sleep(0.1)
+            for b in balances:
+                if b['currency'] != 'KRW':
+                    tkr = "KRW-"+b['currency']
+                    if tkr not in tkr_top10:
+                        if get_balance(tkr,"KRW") > 5000:
+                            sell(tkr)
+                            num_sell += 1
+                        time.sleep(0.1)
+        # 잔고 Update
+            balanceBackup = totalBalance
+            totalBalance = get_totalKRW()
+            if fStart == 0:
+                balanceBackup = totalBalance
+                fStart = 1
+            balance = totalBalance / 10
+            balChange = totalBalance - balanceBackup
+            balChngPercent = balChange / balanceBackup * 100
+        # 1시간 매매 결과 송신
+            post_message(myToken, myChannel, "=== "+str(timeBackup)+"시 ~ "+str(now.hour)+"시 매매 결과 ===")
             post_message(myToken, myChannel, " - 매수 : "+str(num_buy)+"회, 매도 : "+str(num_sell)+"회")
-            post_message(myToken, myChannel, " - 시간 수익 : "+str(round(balChange_hr))+"원 ("+str(round(balChngPercent_hr, 2))+"%)")
-            post_message(myToken, myChannel, " - 금일 수익 : "+str(round(balChange_d))+"원 ("+str(round(balChngPercent_d, 2))+"%)")
+            post_message(myToken, myChannel, " - 시작 잔고 : "+str(round(balanceBackup))+"원")
+            post_message(myToken, myChannel, " - 종료 잔고 : "+str(round(totalBalance))+"원")
+            post_message(myToken, myChannel, " - 수익 : "+str(round(balChange))+"원 ("+str(round(balChngPercent, 2))+"%)")
             num_buy = num_sell = 0
-            hourlyBalance = curBalance
-            balance = curBalance / 10
-
             timeBackup = now.hour
-            fStart = 1
+
 
     # 매매 logic
-        now = datetime.datetime.now()
         for i in range(0, 10):
             tkr = tkr_top10[i]
             # 매수
             if (get_balance(tkr_top10[i],"KRW") < 5000) and balance > 5000:
                 current = get_current_price(tkr)
-                ma15_old = get_ma(tkr, "minute1", 15, 2)
-                ma15 = get_ma(tkr, "minute1", 15, 1)
-                ma30_old = get_ma(tkr, "minute1", 30, 2)
-                ma30 = get_ma(tkr, "minute1", 30, 1)
-                # 기준선보다 높으면 매수
-                if ma15 > ma15_old and ma30 > ma30_old and current > ma30 and now - datetime.timedelta(minutes=10) > last_sell_time[i]:
+                # 목표가보다 상승 시 매수
+                if current > buy_price[i]:
                     buy(tkr, balance)
                     num_buy += 1
             # 매도
             elif get_balance(tkr_top10[i],"KRW") > 5000:
                 current = get_current_price(tkr)
-                ma15 = get_ma(tkr, "minute1", 15, 1)
-                ma30 = get_ma(tkr, "minute1", 30, 1)
-                # 기준선보다 낮으면 매도
-                if current < ma15 or current < ma30 :
+                # 전 시간 종가보다 하락 시 매도
+                if current < sell_price[i]:
                     sell(tkr)
                     num_sell += 1
-                    last_sell_time[i] = now
             time.sleep(0.1)
         time.sleep(1)      
         
