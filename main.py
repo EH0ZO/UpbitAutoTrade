e545240071e0a246471702f9f259321fdb5240a3
Ty <gjxod7901@naver.com>
2021-05-12 21:56:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -69,13 +69,14 @@ while True:
     # 매매 logic
         now = datetime.datetime.now()
         # 전 시간 종가 Update
-        if now.minute % 5 == 0:
-            for i in range(0, 10):
-                close_price[i] = get_close_price(tkr_buy[i])
+        #if now.minute % 5 == 0:
+        #    for i in range(0, 10):
+        #        close_price[i] = get_close_price(tkr_buy[i])
         for i in range(0, 10):
             # if now - datetime.timedelta(minutes=10) > last_trade_time[i]:
             tkr = tkr_buy[i]
             balanceDiff = balance[i] - get_balance(tkr_buy[i],"KRW")
+            close_price[i] = get_close_price(tkr)
             # 매수
             if balanceDiff > 5000:
                 current = get_current_price(tkr)
