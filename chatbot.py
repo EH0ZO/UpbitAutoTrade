4858a0c6b96d09ecf3e61ee613f46991be5f2e1c
Ty <gjxod7901@naver.com>
2021-06-09 17:23:03 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,24 @@
 import telegram
 
+# Global variables
 
+# Keys
+token = "1814838763:AAGNuB_LWtq8zJMHuezB-vsSI8C4b9X9QLk"
+chat_id = 1883488213
+bot = telegram.Bot(token)
 
+# Text
+menu = (
+"-menu-\n"
+"1. current setting\n"
+"2. change setting\n"
+"9. exit"
 
+# Functions
+def send(txt):
+    # 텔레그램 메시지 전송
+    bot.sendMessage(chat_id, txt)        
+    
 def chatbot(t_back):
     recent = bot.getUpdates()[-1].message
     time = recent.date
