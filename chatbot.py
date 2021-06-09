31d62d6954196d94c93dfe487a0f6a1b41ac41f6
Ty <gjxod7901@naver.com>
2021-06-09 17:45:31 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 import telegram
 
 # Global variables
+cur_page = 0
 
 # Keys
 token = "1814838763:AAGNuB_LWtq8zJMHuezB-vsSI8C4b9X9QLk"
@@ -8,11 +9,33 @@ chat_id = 1883488213
 bot = telegram.Bot(token)
 
 # Text
-menu = (
-"-menu-\n"
+# page no.1
+def send_menu():
+    txt = (
+    "== menu ==\n"
+    "1. settings\n"
+    "2. get current \n"
+    "9. exit")
+    send(txt)
+
+# page no.11
+def send_settings():
+    txt = (
+    "current settings\n"
+    "  intv : "+str(intv)+"\n"
+    "  intv : "+str(intv)+"\n"
+    "  intv : "+str(intv)+"\n"
+    "  intv : "+str(intv)+"\n"
+    "0. back\n"
+    "1. change settings\n"
+    "9. exit")
+
+# page no.111
+chg_settings = (
+"0. back\n"
 "1. current setting\n"
 "2. change setting\n"
-"9. exit"
+"9. exit")
 
 # Functions
 def send(txt):
