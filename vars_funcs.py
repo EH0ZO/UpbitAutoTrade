3659a07d105d847b792f31c170783a20fba927f0
Ty <gjxod7901@naver.com>
2021-06-10 13:30:11 +0900

    Update vars_funcs.py

--- vars_funcs.py
+++ vars_funcs.py
@@ -6,7 +6,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 # Global variables
-VERSION = "21.06.10.42"
+VERSION = "21.06.10.43"
 startBalance = 0                    # 09시 기준 잔고
 hourlyBalance = 0                   # 매시 정각 기준 잔고
 bnhBalance = 0                      # 매시 정각 기준 Buy&hold 잔고
