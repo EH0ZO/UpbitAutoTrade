40ade288bc9538816dc32400750abd04f81d4943
HoeTaeYoung <gjxod7901@naver.com>
2021-06-13 11:45:13 +0900

    VERSION = "21.06.12.58"     # 손절 기준 챗봇 추가, 동작 시간 표시 형식 수정

--- vars_funcs.py
+++ vars_funcs.py
@@ -7,7 +7,7 @@ import telegram
 import sys
 
 # Global variables
-VERSION = "21.06.12.58"     # 손절 기준 챗봇 추가, 동작 시간 표시 추가
+VERSION = "21.06.12.58"     # 손절 기준 챗봇 추가, 동작 시간 표시 형식 수정
 # 잔고
 startBalance = 0; hourlyBalance = 0; totalBalance = 0; balanceBackup = 0; balance = 0
 # 매매 횟수
@@ -65,7 +65,7 @@ def send(str):
 def send_start_message():
     txt = "==================================\n"
     txt+= "autotrade start (ver."+VERSION+"))\n"
-    txt+= str(datetime.datetime.now())+"\n"
+    txt+= str(start_time)+"\n"
     txt+= "=================================="
     send(txt)
 
@@ -320,6 +320,7 @@ def send_hourly_report(req):
     balChngPercent_hr = balChange_hr / hourlyBalance * 100
     balChange_d = curBalance - startBalance
     balChngPercent_d = balChange_d / startBalance * 100
+    r_time = datetime.datetime.now() - start_time
     # 결과 송신
     if req == 1:
         txt = "========== Hourly Report ==========\n"
@@ -330,7 +331,7 @@ def send_hourly_report(req):
     txt+= " - 매수(금일) : "+str(num_buy_total)+"회, 매도(금일) : "+str(num_sell_total)+"회\n"
     txt+= " - 수익(시간) : "+str(round(balChange_hr))+"원 ("+str(round(balChngPercent_hr, 2))+"%)\n"
     txt+= " - 수익(금일) : "+str(round(balChange_d))+"원 ("+str(round(balChngPercent_d, 2))+"%)\n"
-    txt+= " - Running  : "+str(datetime.datetime.now() - start_time)
+    txt+= " - Running  : "+str(r_time.hour)+"시간 "+str(r_time.minute)+"분"
     send(txt)
     if req == 1:
         hourlyBalance = curBalance
