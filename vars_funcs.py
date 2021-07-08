75f6240cdd230f54009f1ce57004b2654c150e4d
Ty <gjxod7901@naver.com>
2021-07-08 17:56:36 +0900

    Update vars_funcs.py

--- vars_funcs.py
+++ vars_funcs.py
@@ -605,7 +605,7 @@ def chat(update, context):
             txt+= "5: diff_h : "+str(diff_h)+"\n"
             txt+= "6: diff_l : "+str(diff_l)+"\n"
             txt+= "tkr_num : "+str(tkr_num)+"\n"
-            txt+= "tkr_buy : "+str(tkr_buy[0,tkr_num])+"\n"
+            txt+= "tkr_buy : "+str(tkr_buy[0:tkr_num])+"\n"
             txt+= "stop_trade : "+str(stop_trade)+"\n"
             txt+= "check : "+str(trade_chk)+"\n"
             txt+= "pg version : "+VERSION
